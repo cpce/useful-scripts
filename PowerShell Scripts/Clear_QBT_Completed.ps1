dd87bd8b505dca2c9a9f6dc522c1e7c32b083212
Cody Warmbo <github@warmbo.com>
2019-06-21 10:59:11 -0500

    Add files via upload

---
+++ PowerShell Scripts/Clear_QBT_Completed.ps1
@@ -0,0 +1,30 @@
+# CONFIG
+
+# Torrents removed if Completed this many minutes ago or older
+$PruneAfterMins = 15
+
+# URL of your server including http(s) and port of the WebUI
+$ServerURL = 'http://localhost:8080'
+
+# SCRIPT
+Clear-Host
+$OlderThan = Get-Date (Get-Date).AddMinutes($PruneAfterMins * -1)
+
+$Result = Invoke-RestMethod -Uri "$ServerURL/query/torrents?filter=completed"
+
+foreach ($Torrent in $result) {
+    # timestamps are in UTC
+    $Fin = [timezone]::CurrentTimeZone.ToLocalTime((Get-Date 01.01.1970) + ([System.TimeSpan]::fromseconds($Torrent.completion_on)))
+    $Prune = ($Fin -lt $OlderThan)
+    Write-Output $Torrent.name
+    Write-Output ('Hash = ' + $Torrent.hash)
+    Write-Output ('Finished on ' + $Fin + ' [' + $Torrent.completion_on + ']')
+    Write-Output "Prune = $Prune"
+    Write-Output ""
+
+    If ($Fin) {
+        $Body = ('hashes=' + $Torrent.hash)
+        Invoke-RestMethod -Method Post -Uri "$ServerURl/command/delete" -Body $Body
+    }
+   
+}
